7702603a06906f09e33d899027cf2082c4bfb1b4
Franck-LF <f7lefur@gmail.com>
2025-02-12 14:05:22 +0100

    converting notebook into script

--- script.py
+++ script.py
@@ -13,11 +13,13 @@ import io
 import math
 import copy
 import json
+import pprint
 import requests
 import numpy as np
 import pandas as pd
 from PIL import Image
 import matplotlib.pyplot as plt
+from unidecode import unidecode
 
 from bs4 import BeautifulSoup
 from IPython.display import display
@@ -27,6 +29,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+# MongoDB / Pymongo
+import pymongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
 # One way to set the driver options
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
@@ -34,7 +41,7 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
 def _options():
-    ''' Another way to set the options '''
+    ''' Set Selenium options '''
     options = webdriver.ChromeOptions()
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--test-type')
@@ -46,6 +53,11 @@ def _options():
 
 url_site  = 'https://www.allocine.fr/'
 url_films = 'https://www.allocine.fr/films/'
+
+
+
+
+
 
 
 
